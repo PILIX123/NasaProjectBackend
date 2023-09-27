385d0897ceb8f5e12b0587f43f787bfb1807ffde
Pierre-Luc Robitaille <2012805@cegeplevis.ca>
2023-09-27 00:59:22 -0400

    updated structure of the api key getter

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-from login.login import Login
+from vaultpy.vault import Vault
+from fastapi import FastAPI
 
-Login().login()
+
+app = FastAPI(title="Backend Nasa", docs_url="/nasa/docs",
+              openapi_url="/power/docs/openapi.json")
+
+vault = Vault()
+
+@app.get("/nasa/apod",summary="Get the picture of the day and some other data")
+async def get_apod():
---
+++ vaultpy/vault.py
@@ -0,0 +1,13 @@
+import requests
+import hvac
+import dotenv
+import os
+
+class Vault():
+    def __init__(self) -> None:
+        dotenv.load_dotenv("./vaultconfig/.env")
+        self.client = hvac.Client(url="http://192.168.2.19:8200",token=os.getenv("VAULTTOKEN"))
+    def get_nasa_key(self)->str:
+        read_response=self.client.secrets.kv.read_secret_version(path="nasa")
+        keyValue=read_response['data']['data']['api']
+        return keyValue
